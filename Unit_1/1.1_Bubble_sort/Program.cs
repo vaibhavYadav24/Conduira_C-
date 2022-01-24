c7a4611e6b3d7b8b1bdf65105d47fa94e1c1afc9
vaibhavYadav24 <vaibhavoyadav1@gmail.com>
2022-01-24 21:49:49 +0530

    commit

---
+++ Unit_1/1.1_Bubble_sort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BubbleSort {
+    class Program {
+        static void Main(string[] args) {
+            int[] arr = {8, 4, 9, 2, 1, 6};
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++) {
+                for (int i = 0; i <= arr.Length - 2; i++) {
+                    if (arr[i] > arr[i + 1]) {
+                        temp= arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            foreach (int i in arr)
+                Console.Write(i + " ");
+            Console.Read();
+        }
+    }
+}
