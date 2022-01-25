d778ae3f258b8cc8bb1889b73ad67b984c716352
Vaibhav Yadav <66014367+vaibhavYadav24@users.noreply.github.com>
2022-01-25 19:49:00 +0530

    Add files via upload

---
+++ Unit_1/1.2_string/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "vaibhav";
+        var Frequency = from i in str
+                   group i by i into j
+                   select j;
+        foreach (var arr in Frequency)
+        {
+            Console.WriteLine("Character " + arr.Key + ": " + arr.Count() + " times");
+        }      
+    }
+}
